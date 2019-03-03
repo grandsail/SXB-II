5b4bb770c6b4487cc49616fe6d8094d56ebfd09a
Tanzi1999 <45508331+Tanzi1999@users.noreply.github.com>
2019-03-03 18:59:53 +0800

    “center”终版

--- SXB-II/miniprogram/pages/center/center.js
+++ SXB-II/miniprogram/pages/center/center.js
@@ -32,20 +32,20 @@ Page({
 
 
 
-//按头像跳转至userinfo页面
+//点击头像跳转至userinfo页面
 changeToUserinfo: function () {
   wx.navigateTo({
     url: '../userinfo/userinfo'
   })
 },
 
-//点“常用收件地点”跳转至recievingloc页面
+//点击“常用收件地点”跳转至recievingloc页面
 changeToRecievingloc: function() {
   wx.navigateTo({
     url: '../recievingloc/recievingloc'
   })
 },
-//点“常用收件地点”跳转至history页面
+//点击“常用收件地点”跳转至history页面
 changeTohistory: function () {
   wx.navigateTo({
     url: '../history/history'
