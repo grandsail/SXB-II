15d372f4db9168a412a17b2597b80532b1c0030a
Kumson <36814424+Kumson@users.noreply.github.com>
2019-03-04 04:35:44 +0800

    RL平面表现

--- SXB-II/miniprogram/pages/receivingloc/receivingloc.js
+++ SXB-II/miniprogram/pages/receivingloc/receivingloc.js
@@ -1,66 +1,27 @@
-// pages/receivingloc/receivingloc.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  
   data: {
-
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    // 获取初始收件地点信息
+    this.RefreshData()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  RefreshData : function(){
+    const InitialSearch = wx.cloud.database()
+    InitialSearch.collection('ReceivingLoc').where({
+      _openid: this.data.openid
+    }).get({
+      success: res => {
+        this.setData({
+          RevInfos: res.data
+        })
+        console.log('[数据库] [查询记录] 成功: ', res)
+      },
+      fail: err => {
+        console.error('[数据库] [查询记录] 失败：', err)
+      }
+    })
   }
 })
