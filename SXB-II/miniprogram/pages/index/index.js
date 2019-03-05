c677a03dc3b685cc11641d0c851b00e9c5431928
Kumson <36814424+Kumson@users.noreply.github.com>
2019-03-06 00:59:02 +0800

    index

--- SXB-II/miniprogram/pages/index/index.js
+++ SXB-II/miniprogram/pages/index/index.js
@@ -1,14 +1,3 @@
 Page({
-  data: {
-
-  },
-
-  ToReceivingloc : function(){
-    wx.navigateTo({
-      url: '../receivingloc/receivingloc',
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
-    })
-  }
+  
 })
