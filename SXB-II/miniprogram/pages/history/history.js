f05cae3062a255318f0679160613cf71cbd7ab93
sxb666 <45492056+sxb666@users.noreply.github.com>
2019-03-24 20:14:19 +0800

    3.24-2

---
+++ SXB-II/miniprogram/pages/history/history.js
@@ -0,0 +1,42 @@
+// miniprogram/pages/test/test.js
+Page({
+  onLoad: function (options) {
+    this.onQuery()
+  },
+  data: {
+    focus: false,
+    inputValue: '',
+    queryResult: '',
+    count: 0,
+    id: ''
+  },
+  //查找
+  onQuery: function () {
+    const db = wx.cloud.database()
+    // 查询当前用户所有的 counters
+    db.collection('Orders').where({
+      _openid: this.data.openid
+    }).get({
+      success: res => {
+        this.setData({
+          queryResult: res.data
+        })
+        console.log('[数据库] [查询记录] 成功: ', res)
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '查询记录失败'
+        })
+        console.error('[数据库] [查询记录] 失败：', err)
+      }
+    })
+  },
+  //跳转
+  detail: function (e) {
+    let id = e.currentTarget.dataset.id
+    wx.navigateTo({
+      url: '../detail/detail?id=' + id,
+    })
+  },
+})
